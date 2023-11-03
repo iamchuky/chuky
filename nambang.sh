934dcc27d0041b307e2a0b6ece8ce0c9bf22c166
iamchuky <149824475+iamchuky@users.noreply.github.com>
2023-11-04 01:47:36 +0700

    nambang.sh

---
+++ nambang.sh
@@ -0,0 +1,4 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar vxf hellminer_cpu_linux.tar.gz
+cd hellminer_cpu_linux
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RAiciGDhD94bgh5jbisqL6KYvA5FHdZPGf.Hell01 -p x --cpu 4
